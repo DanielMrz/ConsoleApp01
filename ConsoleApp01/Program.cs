6255cfec54dcf0f93aa78eb46a033b1faa652a10
bufor <bufor@DESKTOP-8S4KUFF>
2022-09-26 22:13:20 +0200

    Add project files.

---
+++ ConsoleApp01/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj wartość");
+        var value = Console.ReadLine();
+        Console.WriteLine($"Twoja wartość: {value}");
+        Console.WriteLine($"{DateTime.Now:dd-MM-yyy HH:mm} {DateTime.Now.DayOfWeek}");
+
+        var random = new Random();
+        Console.WriteLine($"Losowa liczba: {random.Next(0, 10)}");
+       Console.ReadLine();
+    }
+}
